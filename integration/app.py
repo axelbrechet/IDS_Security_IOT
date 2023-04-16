bbec847bc74ceaf7c6b0af80f837721341f09e79
vekxze <vekxze@proton.me>
2023-04-15 21:53:20 -0400

    remove ring buffer

--- integration/app.py
+++ integration/app.py
@@ -7,14 +7,15 @@ from notify.notify import Notify
 sniffer = Sniffer('\\Device\\NPF_{718ADC81-1EAC-4ED0-835F-1E96B7DF7076}')
 model = load_model('saved_model.h5')
 
-def analyze(buffer):
-    data = np.vstack([list(vars(pkt).values()) for pkt in buffer])
-    data = data.reshape(1, data.shape[0], data.shape[1])
-
+def analyze(pkt):
+    values = list(vars(pkt).values())
+    data = np.array(values).reshape(1,1,len(values))
     yhat = model.predict(data, verbose=0)
-    probability = yhat[0][0]    
-    if probability > 0.6:
-        print(f'Attack detected with probability {probability:.2f}')
+    probability = yhat[0][0]
+    print(probability)
+    #if probability > 0.6:
+        #print(f'Attack detected with probability {probability:.2f}')
         #Notify.send_alert("Unknown attack", probability)
 
+        
 sniffer.run(callback=analyze)
--- integration/sniffer.py
+++ integration/sniffer.py
@@ -2,62 +2,31 @@ import pyshark
 import time
 from dataclasses import dataclass
 
-# Circular Buffer
-class Buffer():
-
-    def __init__(self, size):
-        self.size = size
-        self.buffer = [None] * size
-        self.idx = 0
-
-    def __iter__(self):
-        idx = 0
-        while idx < self.size:
-            yield self.buffer[idx]
-            idx += 1
-
-    def __str__(self):
-        result = '-' * 15 + '\n'
-        for idx, item in enumerate(self.buffer):
-            result += f'{idx}: {item}\n'
-        result += '-' * 15 + '\n'
-        return result
-
-    def is_full(self):
-        return self.buffer[self.size-1] is not None
-
-    def add(self, elem):
-        self.buffer[self.idx] = elem
-        self.idx = (self.idx + 1) % self.size
-
 # Immutable data class
 @dataclass(frozen=True)
 class Packet():
-    srcaddr : int # IPV4 source address (decimal integers)
-    dstaddr : int # IPV4 destination address (decimal integers)
-    type    : int # Layer 3 protocol (decimal integers) => IPV4:0x0800=2048
-    length  : int
-    time    : int 
-    srcport : int
-    dstport : int
+    srcaddr     : int
+    dstaddr     : int
+    length      : int
+    protocol    : int 
+    srcport     : int
+    dstport     : int
+    timediff    : int 
 
 class Sniffer:
 
-    BUFFER_SIZE = 9 # Should be equal to the time_steps in the model
-
     def __init__(self, iface):
         self.capture = pyshark.LiveCapture(interface=iface)
-        self.buffer = Buffer(Sniffer.BUFFER_SIZE)
+        self.last_pkt_time = None
 
     def run(self, callback):
         for raw_pkt in self.capture.sniff_continuously():
             if not (self.filter(raw_pkt)): continue
             pkt = self.build_packet(raw_pkt)
-            self.buffer.add(pkt)
-            if self.buffer.is_full(): 
-                callback(self.buffer)
+            self.last_pkt_time = time.time()
+            callback(pkt)
             
-    def filter(self, raw_pkt):  
+    def filter(self, raw_pkt):
         is_ipv4 = raw_pkt.eth.type == '0x0800'
         is_ipv6 = raw_pkt.eth.type == '0x86dd'
         is_tcp = raw_pkt.transport_layer == 'TCP'
@@ -67,9 +36,9 @@ class Sniffer:
         return Packet(
             raw_pkt.ip.src.hex_value,
             raw_pkt.ip.dst.hex_value,
-            raw_pkt.eth.type.hex_value,
             len(raw_pkt),
-            int(time.time()),
+            raw_pkt.ip.proto.hex_value,
             raw_pkt.tcp.srcport.hex_value,
             raw_pkt.tcp.dstport.hex_value,
+            0 if self.last_pkt_time is None else time.time() - self.last_pkt_time,
         )
